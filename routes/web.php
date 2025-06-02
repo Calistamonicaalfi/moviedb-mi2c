aaefb909916847d04687a4d471afdbfa26992007
Calistamonicaalfi <161101838+Calistamonicaalfi@users.noreply.github.com>
2025-06-02 16:49:52 +0700

    login dan logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +15,8 @@ Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movie.detail'
 // Menampilkan form
 Route::get('/create-movie', [MovieController::class, 'create']);
 Route::post('/create-movie', [MovieController::class, 'store']);
+
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
